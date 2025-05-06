ee6fd5ec0e04c2fe5555f60113a8f43915a5e4f5
Robson Alves <contato@robsonalves.dev.br>
2025-05-06 08:13:35 -0300

    Fix directory check in recursive git pull script to ensure it verifies the presence of a .git folder

--- recursive-git-pull.sh
+++ recursive-git-pull.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 for nome in */; do
-  if [[ -d "$nome" ]]; then
+  if [[ -d "$nome/.git" ]]; then
     cd "$nome" || exit
-    git status | grep -q "working tree clean"
-    if [[ $? -eq 0 ]]; then
-      echo -e "\e[00;32mO comando 'algum_comando' teve sucesso em $nome. O status do Git é:\e[00m"
+    #git status | grep -q "working tree clean"
+    #if [[ $? -eq 0 ]]; then
+    echo -e "\e[01;33mVerificando $nome.\e[00m"
+    if git status | grep -q "working tree clean"; then
       git pull
+      echo -e "\e[00;32mO comando 'git status' teve sucesso em $nome.\e[00m\n\n"
     else
-      echo -e "\e[00;31mO comando 'algum_comando' falhou em $nome. Nenhuma ação adicional será tomada.\e[00m"
+      echo -e "\e[00;31mO comando 'git status' falhou em $nome. Nenhuma ação adicional será tomada.\e[00m\n\n"
     fi
     cd - > /dev/null 2>&1 || exit
   fi
